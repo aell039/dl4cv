72adb2587038d1ae45da21a8e88cc260932f3ffa
aell039 <8273272+aell039@users.noreply.github.com>
2019-12-24 01:56:48 -0500

    applying backprop to XOR dataset

---
+++ nn_xor.py
@@ -0,0 +1,20 @@
+from pyimagesearch.nn import NeuralNetwork
+import numpy as np
+
+# construct the XOR dataset
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [1], [1], [0]])
+
+# define a 2-2-1 neural network and train it
+nn = NeuralNetwork([2, 2, 1], alpha=0.5)
+nn.fit(X, y, epochs=20000)
+
+# make predictions for the XOR data ponts, applying a step function to the sigmoid output
+for (x, target) in zip(X, y):
+    pred = nn.predict(x)[0][0]
+    step = 1 if pred > 0.5 else 0
+    print(
+        "[INFO] data={}, ground-truth={}, pred={:.4f}, step={}".format(
+            x, target[0], pred, step
+        )
+    )
